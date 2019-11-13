f19df0475f6af7cd31d78873b8874c36fdf8690d
Ryan Rightmer <rrightmer@brighthealthplan.com>
2019-11-13 15:01:31 -0600

    Search work

--- pages/search.tsx
+++ pages/search.tsx
@@ -2,26 +2,42 @@ import Head from 'next/head'
 import fetch from 'isomorphic-unfetch'
 import Layout from '../components/layout'
 
-const Search = () => {
+type BasicSearchResult = {
+  st_dist: number,
+  pl_hostname: string,
+  pl_name: string,
+}
+
+const Search = (props: { data: BasicSearchResult[] }) => {
+  console.log(props.data)
   return (
     <Layout>
       <Head>
         <title>Search</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      
 
       <p>Search for another world</p>
+      <ul>
+        {
+          props.data.map(x => { <li>{x.st_dist}: {x.pl_name}</li>})
+        }
+      </ul>
+      
     </Layout>
   );
 }
 
+
+
 Search.getInitialProps = async function() {
-  const res = await fetch('https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?&table=exoplanets&format=json&where=pl_kepflag=1');
-  const data = await res.json();
+  const res = await fetch('https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?&table=exoplanets&format=json&select=st_dist,pl_hostname,pl_name&order=st_dist');
+  const data: BasicSearchResult[] = await res.json();
 
   console.log(`Show data fetched. Count: ${data.length}`);
 
-  return data;
+  return { data };
 };
 
 export default Search;
